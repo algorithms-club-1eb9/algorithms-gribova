716502b03ed71473fa3b8e093aaf0c945cb3857c
lizagrib25 <lizagrib25@gmail.com>
2018-03-17 22:09:27 +0200

    [Linked List] Added linked list implementation and tests Fixed tests for DCP and changed depth on count in implementation Added Stack - TO DO: Tests Started working on Queue

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const binarySearch = require('./lib/algorithms/binary-search');
 const DCP = require('./lib/algorithms/dynamic-connectivity-problem');
+const Stack = require('./lib/data-strucrutes/stack');
+const Queue = require('./lib/data-strucrutes/queue');
+const LinkedList = require('./lib/data-strucrutes/linked-list');
+
+
+getUniqueItem = function getUniqueItem(count, notEqual) {
+    const _result = Math.floor(Math.random() * count);
+    return _result === notEqual ? getUniqueItem(count, notEqual) : _result;
+};
 
 module.exports = {
     binarySearch,
     DCP,
+    Stack,
+    Queue,
+    LinkedList,
+    getUniqueItem,
 };
